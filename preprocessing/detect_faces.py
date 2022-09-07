5ffa0200dc96e78f801eb743f2987732d0b76ef0
ardn92 <ardn92@gmail.com>
2022-09-07 11:09:02 +0430

    debug prints added to face detection

--- preprocessing/detect_faces.py
+++ preprocessing/detect_faces.py
@@ -1,7 +1,8 @@
 import argparse
 import json
-# from multiprocessing import set_start_method    # local
+from multiprocessing import set_start_method    # local
 import os
+import sys
 from random import shuffle
 import numpy as np
 from typing import Type
@@ -18,50 +19,58 @@ import torch
 
 
 def process_videos(videos, detector_cls: Type[VideoFaceDetector], selected_dataset, opt):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    detector = face_detector.__dict__[detector_cls](device=device)
+    try:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        detector = face_detector.__dict__[detector_cls](device=device)
 
-    dataset = VideoDataset(videos)
+        dataset = VideoDataset(videos)
 
-    # loader = DataLoader(dataset, shuffle=False, num_workers=12, batch_size=1, collate_fn=lambda x: x)    # local
-    loader = DataLoader(dataset, shuffle=False, num_workers=opt.processes, batch_size=int(opt.batch_size), collate_fn=lambda x: x)
-    missed_videos = []
-    for item in tqdm(loader): 
-        result = {}
-        video, indices, frames = item[0]
-        print("processing... :", video, len(indices), frames)
-        if selected_dataset == 1:
-            method = get_method(video, opt.data_path)
-            if opt.output == "":
-                out_dir = os.path.join(opt.data_path, "boxes", method)
+        loader = DataLoader(dataset, shuffle=False, num_workers=12, batch_size=1, collate_fn=lambda x: x)    # local
+        # loader = DataLoader(dataset, shuffle=False, num_workers=opt.processes, batch_size=int(opt.batch_size), collate_fn=lambda x: x)
+        missed_videos = []
+        for item in tqdm(loader): 
+            result = {}
+            video, indices, frames = item[0]
+            print("processing... :", video, len(indices))
+            if selected_dataset == 1:
+                method = get_method(video, opt.data_path)
+                if opt.output == "":
+                    out_dir = os.path.join(opt.data_path, "boxes", method)
+                else:
+                    out_dir = os.path.join(opt.output, "boxes", method)
             else:
-                out_dir = os.path.join(opt.output, "boxes", method)
-        else:
-            out_dir = os.path.join(opt.data_path, "boxes")
+                out_dir = os.path.join(opt.data_path, "boxes")
 
-        id = os.path.splitext(os.path.basename(video))[0]
+            id = os.path.splitext(os.path.basename(video))[0]
 
-        if os.path.exists(out_dir) and "{}.json".format(id) in os.listdir(out_dir):
-            continue
-        batches = [frames[i:i + detector._batch_size] for i in range(0, len(frames), detector._batch_size)]
-        for j, frames in enumerate(batches):
-            result.update({int(j * detector._batch_size) + i : b for i, b in zip(indices, detector._detect_faces(frames))})
+            if os.path.exists(out_dir) and "{}.json".format(id) in os.listdir(out_dir):
+                continue
+            print("creating batches...")
+            batches = [frames[i:i + detector._batch_size] for i in range(0, len(frames), detector._batch_size)]
+            print("batch created")
+            for j, frames in enumerate(batches):
+                result.update({int(j * detector._batch_size) + i : b for i, b in zip(indices, detector._detect_faces(frames))})
+            print("looped over batches. batch stats :", len(batches))
         
-       
-        os.makedirs(out_dir, exist_ok=True)
-        print(len(result))
-        if len(result) > 0:
-            with open(os.path.join(out_dir, "{}.json".format(id)), "w") as f:
-                json.dump(result, f)
-        else:
-            missed_videos.append(id)
+            os.makedirs(out_dir, exist_ok=True)
+            print('result: ', len(result))
+            if len(result) > 0:
+                print('writing results to dir - stats: (len, size)', len(result), sys.getsizeof(result))
+                with open(os.path.join(out_dir, "{}.json".format(id)), "w") as f:
+                    json.dump(result, f)
+                print('writing done')
+            else:
+                missed_videos.append(id)
+                print('missed video; id: ', id, "result: ", result)
 
-    if len(missed_videos) > 0:
-        print("The detector did not find faces inside the following videos:")
-        print(id)
-        print("We suggest to re-run the code decreasing the detector threshold.")
+        if len(missed_videos) > 0:
+            print("The detector did not find faces inside the following videos:")
+            print(id)
+            print("We suggest to re-run the code decreasing the detector threshold.")
 
 
+    except Exception as e:
+        print('Error Occured: ', e)
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', default="DFDC", type=str,
@@ -123,5 +132,5 @@ def main():
 
 
 if __name__ == "__main__":
-    # set_start_method('fork')    # local
+    set_start_method('fork')    # local
     main()
